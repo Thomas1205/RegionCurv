96fef7066d88d3556320bc5266512368e84553de
Petter Strandmark <petter.strandmark@gmail.com>
2011-09-05 16:02:39 +0200

    Added GPC to download script

--- download_thirdparty.sh
+++ download_thirdparty.sh
@@ -9,6 +9,11 @@ mv tmp/Image.h HOCR/Image.h
 mv tmp/HOCR/HOCR.h HOCR/HOCR.h
 mv tmp/HOCR/HOCR0.h HOCR/HOCR0.h
 
+wget ftp://ftp.cs.man.ac.uk/pub/toby/gpc/gpct101.zip
+unzip gpct101.zip -d tmp
+mv tmp/gpct101/gpc.c thirdparty/gpc.c
+mv tmp/gpct101/gpc.h thirdparty/gpc.h
+
 rm QPBO-v1.3.src.tar.gz
 rm HOCR1.02.zip
 rm -r tmp
